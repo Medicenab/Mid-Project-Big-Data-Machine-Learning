5ebcf1f8e4a003e4a6de29c513695ee0246bd6ab
medicenab <abrahamjose.sg@gmail.com>
2022-05-30 01:04:00 -0500

    version #2

--- DASHBOARD/DATA/get_data.py
+++ DASHBOARD/DATA/get_data.py
@@ -12,9 +12,11 @@ def get_all():
 def data_clean(data,year_select,dist):
     for_graph={}
     for x in data:
-        if x["Year"]==year_select:
-            if x['District Name']==dist:
-                for_graph[x['Nationality']]=(x['Number'])
+        for fecha in year_select:
+            if x["Year"]==fecha:
+                if x['District Name']==dist:
+                    for_graph[x['Nationality']]=(x['Number'])
+        return  for_graph           
     for_graph=dict( sorted(for_graph.items(),
                            key=lambda item: item[1],
                            reverse=True))
--- DASHBOARD/main.py
+++ DASHBOARD/main.py
@@ -5,24 +5,84 @@ from DATA.geo import map_dis
 
 
 st.title('Inmigracion en Barcelona')
-st.text('Distribucion de nacionalidades por distrito en BCN')
-
+st.text('Distribucion de nacionalidades por distrito en BCN, durante 2015, 2016 y 2017')
+year=get_year()
+year_select = st.multiselect('Esgoje un año', [x for x in year])
 
 BCN=get_dis()
-print (type((BCN)))
-dist = st.selectbox('Esgoje un distrito', [x for x in BCN])
-print (dist)
 
-year=get_year()
-year_select = st.selectbox('Esgoje un año', [x for x in year])
-print(year_select)
-print(type(year_select))
+dist = st.selectbox('Esgoje un distrito', [x for x in BCN])
+
+
+for_graph={}
+for_graph_1={}
+for_graph_2={}
 
 all_data=get_all()
 
-for_graph=data_clean(all_data,year_select,dist)
+if len(year_select)==1:
+    for_graph={}
+    for x in all_data:
+        if x["Year"]==year_select[0]:
+                if x['District Name']==dist:
+                    for_graph[x['Nationality']]=(x['Number'])
+    for_graph=dict( sorted(for_graph.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph)                                       
+if len(year_select)==2:
+    for_graph={}
+    for x in all_data:
+        if x["Year"]==year_select[0]:
+                if x['District Name']==dist:
+                    for_graph[x['Nationality']]=(x['Number'])
+    for_graph=dict( sorted(for_graph.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph)  
+    for_graph_1={}
+    for x in all_data:
+        if x["Year"]==year_select[1]:
+                if x['District Name']==dist:
+                    for_graph_1[x['Nationality']]=(x['Number'])
+    for_graph_1=dict( sorted(for_graph_1.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph_1)
+if len(year_select)==3:
+    for_graph={}
+    for x in all_data:
+        if x["Year"]==year_select[0]:
+                if x['District Name']==dist:
+                    for_graph[x['Nationality']]=(x['Number'])
+    for_graph=dict( sorted(for_graph.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph)  
+    for_graph_1={}
+    for x in all_data:
+        if x["Year"]==year_select[1]:
+                if x['District Name']==dist:
+                    for_graph_1[x['Nationality']]=(x['Number'])
+    for_graph_1=dict( sorted(for_graph_1.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph_1)
+    for_graph_2={}
+    for x in all_data:
+        if x["Year"]==year_select[2]:
+                if x['District Name']==dist:
+                    for_graph_2[x['Nationality']]=(x['Number'])
+    for_graph_2=dict( sorted(for_graph_2.items(),
+                           key=lambda item: item[1],
+                           reverse=True))
+    pieChart(for_graph_2)
 
-pieChart(for_graph)
+
+
+#for_graph=data_clean(all_data,year_select,dist)
+
+
 
 df=map_dis(dist)
 st.map(df)
